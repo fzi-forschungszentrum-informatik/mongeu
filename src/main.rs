78afa47762c27f359e2dc23e4414ff977ab7548c
Julian Ganz <ganz@fzi.de>
2024-03-21 00:22:15 +0100

    Add a convenience trait for transforming stuff into a Reply
    We intend to write a small HTTP service using `warp`. It's purpose is to
    expose some information on local GPUs to other services via a REST API.
    This will, in almost all cases, mean that we get some value which needs
    to be serialized. Additionally, when querying those values, we may
    encounter various errors that we need to handle. In short, we need to
    generate an appropriate `warp::Reply` from a `Result` in quite a few
    places.
    
    While this may be somewhat straght-forward, it involves some amount of
    boiler-plate code. In order to minimize that, this change introduces a
    convenience trait with an impl for `Result`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
 }
+
+/// Convenience trait for transforming stuff into a [warp::Reply]
+trait Replyify {
+    /// Transform this value into a [warp::Reply]
+    fn replyify(self) -> impl warp::Reply;
+}
+
+impl<T: serde::Serialize, E: ToString> Replyify for Result<T, E> {
+    fn replyify(self) -> impl warp::Reply {
+        use warp::http::StatusCode;
+
+        self.as_ref()
+            .map(warp::reply::json)
+            .map_err(|e| warp::reply::with_status(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))
+    }
+}
