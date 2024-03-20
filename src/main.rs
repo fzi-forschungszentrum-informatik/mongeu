b1cc8a22eb773e4ed44548135851785d71af8f2e
Julian Ganz <ganz@fzi.de>
2024-03-21 00:42:10 +0100

    Extend to an MVP which allows querying the number of nvidia GPUs
    This change introduces some basic setup which we'll extend in the near
    future.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use std::sync::Arc;
+
+use nvml_wrapper as nvml;
+
+use nvml::Nvml;
+use warp::Filter;
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> Result<(), nvml::error::NvmlError> {
+    let nvml = Nvml::init().map(Arc::new)?;
+
+    // End-point exposing the number of devices on this machine
+    let device_count = warp::get()
+        .and(warp::path("device_count"))
+        .and(warp::path::end())
+        .map({
+            let nvml = nvml.clone();
+            move || nvml.device_count().replyify()
+        });
+
+    let v1_api = device_count;
+    let v1_api = warp::any().and(warp::path("v1")).and(v1_api);
+
+    warp::serve(v1_api).run(([0, 0, 0, 0], 8080)).await;
+    unreachable!()
 }
 
 /// Convenience trait for transforming stuff into a [warp::Reply]
