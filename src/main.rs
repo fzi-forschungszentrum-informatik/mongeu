424b535f40f2105b13dc1d141bcdb3cf2b7b8a5c
Julian Ganz <ganz@fzi.de>
2024-03-21 22:26:17 +0100

    Impl Replyify for Result<_, Replyify> rather than Result<_, ToString>
    We recently implemented `Replyify` for `NvmlError`, which is currently
    the only thing we ever shove into the `Err` variant of a `Result` on
    which we call `Replyify` functions. And this will likely not change too
    dramatically in the future.
    
    This offers the opportunity to base the impl for `Result` on `Replyify`
    rather than on `ToString`. This brings a number of benefits, most
    notably reduced code duplication.

--- src/main.rs
+++ src/main.rs
@@ -104,13 +104,10 @@ trait Replyify {
     fn replyify(self) -> impl warp::Reply;
 }
 
-impl<T: serde::Serialize, E: ToString> Replyify for Result<T, E> {
+impl<T: serde::Serialize, E: Replyify> Replyify for Result<T, E> {
     fn replyify(self) -> impl warp::Reply {
-        use warp::http::StatusCode;
-
-        self.as_ref()
-            .map(warp::reply::json)
-            .map_err(|e| warp::reply::with_status(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))
+        self.map(|v| warp::reply::json(&v))
+            .map_err(Replyify::replyify)
     }
 }
 
