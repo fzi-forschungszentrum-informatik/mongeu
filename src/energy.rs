7cd108a5ff90cacf0f212a123c29b7b1b5377430
Julian Ganz <ganz@fzi.de>
2024-03-24 10:36:15 +0100

    energy: add transfer data types
    We intend to provide some more sophisticated endpoints for exposing the
    GPUs' power consmuption over time. The measurement results are strucuted
    and contain various elements which cannot be disentagled sensibly.
    
    This change introduces transfer data structures for that purpose.

--- src/energy.rs
+++ src/energy.rs
@@ -1,1 +1,19 @@
 //! Energy consumption measurement and associated utilities
+
+/// A measurement across multiple devices
+#[derive(Debug, serde::Serialize)]
+pub struct Measurement {
+    /// Time passed since the start of a campaign in `ms`
+    time: u128,
+    /// Device data at the specific point in time
+    devices: Vec<DeviceData>,
+}
+
+/// Data associated with a specific device
+#[derive(Copy, Clone, Debug, serde::Serialize)]
+pub struct DeviceData {
+    /// Index of the device
+    id: u32,
+    /// Energy consumption of the device in `mJ`
+    energy: u64,
+}
