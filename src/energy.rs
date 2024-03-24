d8f211ecad8fc0c5c98fb04a286b2ca614fc4b50
Julian Ganz <ganz@fzi.de>
2024-03-24 10:36:16 +0100

    energy: add fns for creating total and relative DeviceData
    Although `DeviceData` is primarily meant as a transfer type, we decided
    to also use it for longterm-ish storage for base-data. Thus, we end up
    with one use where `energy` denotes the _toal_ energy consumption of a
    device since boot/startup and one use where it denotes the consumtion
    _relative_ to another measurement.
    
    We plan to create new "total" `DeviceData` and create "relative"
    `DeviceData` from it later on. This change introduces fns which let you
    do that, as well as some convenience fns for wrapping `NvmlError`s.
    Since they are meant for use in `energy` only, we decided it was
    acceptable for `total_energy_consumption` take the device id as an
    argument despite also receiving the `Device` associated with it in order
    to construct more expressive context information.

--- src/energy.rs
+++ src/energy.rs
@@ -1,4 +1,6 @@
 //! Energy consumption measurement and associated utilities
+use anyhow::{Context, Result};
+use nvml_wrapper as nvml;
 
 /// A measurement across multiple devices
 #[derive(Debug, serde::Serialize)]
@@ -16,4 +18,38 @@ pub struct DeviceData {
     id: u32,
     /// Energy consumption of the device in `mJ`
     energy: u64,
+}
+
+impl DeviceData {
+    /// Create a new total device data for the current time instant
+    pub fn new_total(device: nvml::Device) -> Result<Self> {
+        let id = device
+            .index()
+            .context("Could not determine index of device")?;
+        let energy = total_energy_consumption(device, id)?;
+        Ok(Self { id, energy })
+    }
+
+    /// Compute new relative device data from total device data
+    pub fn relative(self, nvml: &nvml::Nvml) -> Result<Self> {
+        device_by_index(nvml, self.id)
+            .and_then(|d| total_energy_consumption(d, self.id))
+            .map(|e| Self {
+                energy: e.saturating_sub(self.energy),
+                ..self
+            })
+    }
+}
+
+/// [nvml::Nvml::device_by_index] with [Context]
+fn device_by_index(nvml: &nvml::Nvml, id: u32) -> Result<nvml::Device> {
+    nvml.device_by_index(id)
+        .with_context(|| format!("Could not retrieve device {id}"))
+}
+
+/// [nvml::Device::total_energy_consumption] with [Context]
+fn total_energy_consumption(device: nvml::Device, id: u32) -> Result<u64> {
+    device
+        .total_energy_consumption()
+        .with_context(|| format!("Could not retrieve total energy consumption of device {id}"))
 }
