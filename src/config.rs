326ffc8457c72eaa4204f9a537ba55aa1c1dcd3a
Julian Ganz <ganz@fzi.de>
2024-04-01 13:53:07 +0200

    config: add type representing an address to listen on for connections
    We intend to allow configuration via a config file. In the case of
    network configuration, this will allow for more fine-grained control: on
    the CLI, we can only provide one port and a set of IPs to listen on, if
    we don't want to introduce some cumbersome, special CLI scheme. However,
    in a TOML file we can just have an array of maps.
    
    This change introduces a type allowing to express the latter while
    supporting the CLI use-case. The type's deserialize method will allow
    parsing configuration either with an explicit port for a given IP or
    using a default port, potentially from some other source. We also supply
    a `FromStr` implementation which will make it much easier to use with
    `clap`'s derive macros.

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,9 @@
 //! Configuration related types and utilities
-use std::net::IpAddr;
+use std::net::{IpAddr, SocketAddr};
 use std::num::NonZeroUsize;
 use std::time::Duration;
+
+use serde::Deserialize;
 
 pub const DEFAULT_LISTEN_ADDRS: [IpAddr; 2] = [
     IpAddr::V4(std::net::Ipv4Addr::UNSPECIFIED),
@@ -13,3 +15,32 @@ pub const DEFAULT_ONESHOT_DURATION: Duration = Duration::from_millis(500);
 
 pub const DEFAULT_GC_MIN_AGE: Duration = Duration::from_secs(24 * 60 * 60);
 pub const DEFAULT_GC_MIN_CAMPAIGNS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(1 << 16) };
+
+/// One single address to listen on
+#[derive(Copy, Clone, Debug, PartialEq, Deserialize)]
+pub struct ListenAddr {
+    /// IP addr to listen on
+    ip: IpAddr,
+    /// port to listen on (overriding the default port)
+    port: Option<u16>,
+}
+
+impl ListenAddr {
+    /// Create a new [ListenAddr] from an [IpAddr]
+    const fn new(ip: IpAddr) -> Self {
+        Self { ip, port: None }
+    }
+
+    /// Transform into a [SocketAddr], using the specified default for the port
+    pub fn socket_addr(self, default_port: u16) -> SocketAddr {
+        SocketAddr::new(self.ip, self.port.unwrap_or(default_port))
+    }
+}
+
+impl std::str::FromStr for ListenAddr {
+    type Err = <IpAddr as std::str::FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        IpAddr::from_str(s).map(Self::new)
+    }
+}
