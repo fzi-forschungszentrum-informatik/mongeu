320a1e914869df15f8f37d52f164bd4fb17dc128
Julian Ganz <ganz@fzi.de>
2024-04-03 22:14:23 +0200

    util: add fns for parsing Durations from numeric string reps
    We try to clean up our configuration retrieval code, and use the actual
    types we want rather than some proxy that we need to transform. For this
    purpose, this change introduces some rather trivial fns which can be
    used as `clap::builder::TypedValueParser`.

--- src/util.rs
+++ src/util.rs
@@ -1,1 +1,13 @@
 //! Utilities
+use std::num::{NonZeroU64, ParseIntError};
+use std::time::Duration;
+
+/// Parse a non-zero [Duration] provided in milliseconds
+pub fn parse_millis(s: &str) -> Result<Duration, ParseIntError> {
+    s.parse().map(NonZeroU64::get).map(Duration::from_millis)
+}
+
+/// Parse a non-zero [Duration] provided in seconds
+pub fn parse_secs(s: &str) -> Result<Duration, ParseIntError> {
+    s.parse().map(NonZeroU64::get).map(Duration::from_secs)
+}
