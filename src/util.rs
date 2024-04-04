0c129c155750340b6c89390b2309e25259d74e3c
Julian Ganz <ganz@fzi.de>
2024-04-04 18:10:21 +0200

    util: add fn for serializing an Duration as number of milliseconds
    For configuration parsing, we recently introduced some utility fns to be
    used with `#[serde(deserialize_with = ...)]`. We did so because we
    wanted the data in the actual type we need, rather than in a proxy type
    which happens to deserialize, but needs some conversion afterwards.
    
    The same can be said about types we process as part of requests and
    responses. One such instance would be the `energy::Measurement` which
    contains an `u128` which really should have been a `Duration`.
    
    This change introduces a `fn` to use with `serialize_with = ...`. A
    future change will wire it up.

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@
 use std::num::{NonZeroU64, ParseIntError};
 use std::time::Duration;
 
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 /// Parse a non-zero [Duration] provided in milliseconds
 pub fn parse_millis(s: &str) -> Result<Duration, ParseIntError> {
@@ -12,6 +12,14 @@ pub fn parse_millis(s: &str) -> Result<Duration, ParseIntError> {
 /// Parse a non-zero [Duration] provided in seconds
 pub fn parse_secs(s: &str) -> Result<Duration, ParseIntError> {
     s.parse().map(NonZeroU64::get).map(Duration::from_secs)
+}
+
+/// Serialize a [Duration] as a number of milliseconds
+pub fn serialize_millis<S: Serializer>(
+    duration: &Duration,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
+    duration.as_millis().serialize(serializer)
 }
 
 /// Deserialize a non-zero [Duration] provided in milliseconds
